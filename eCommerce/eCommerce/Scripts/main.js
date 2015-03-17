ada3a88d52c85f27b826491ac281377412d7f86c
Andrii Pelekh <pelekhav@gmail.com>
2015-03-16 21:39:57 -0600

    Changes 3-16-15

---
+++ eCommerce/eCommerce/Scripts/main.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $(".thumbnail img").on("mouseover", function () {
+        $("img.main-image").attr("src", $(this).attr("src"));
+    });
+});
+
+//step 1.bind an event to mouse
+$(document).ready(function () {
+    $("body").on("mouseover", ".thumbnail", function () {
+        //step 2.when we mouseover a thumbnail, we will want to set the main image src to the thumbnail we moused over
+        var theDivWeMousedOver = $(this);
+        var theMainImage = $("img.main-image");
+        var theImageWeMousedOver = theDivWeMousedOver.children("img");
+        var theSRCOfTheImageWeMousedOver = theImageWeMousedOver.attr("src");
+        console.log(theSRCOfTheImageWeMousedOver);
+
+        //sets the src of the mail image to the src of the image we moused over
+        theMainImage.attr("src", theSRCOfTheImageWeMousedOver);
+
+    });
+
+
+});
